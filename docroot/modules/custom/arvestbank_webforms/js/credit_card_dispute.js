d46bad62c3bde380dcd8d01b1fc0df40f1f0120e
Scott Earnest <searnest@elevatedthird.com>
2021-03-29 16:09:06 -0600

    AB-45: Credit card dispute form setup.

---
+++ docroot/modules/custom/arvestbank_webforms/js/credit_card_dispute.js
@@ -0,0 +1,19 @@
+(function ($, Drupal) {
+
+  'use strict';
+
+  $('input.nature-of-dispute').on('click', function (e) {
+
+    // Uncheck and hide all dispute type form and sections.
+    $('input.nature-of-dispute').each(function () {
+      $(this).prop('checked', false);
+      $(this).trigger('change');
+    });
+
+    // Go back and check and un-hide this dispute form.
+    $(this).prop('checked', true);
+    $(this).trigger('change');
+
+  });
+
+})(jQuery, Drupal);
