794ff7c85cc1a0ac4399bbda60fda834d69e0126
John Cunningham <drfirepants@2954699.no-reply.drupal.org>
2021-05-04 10:21:36 -0400

    AB-63: SSo fixes and cleanup.

--- simplesamlphp/config/authsources.php
+++ simplesamlphp/config/authsources.php
@@ -1,14 +1,14 @@
 <?php
 
-$config = array(
-  'admin' => array(
+$config = [
+  'admin' => [
     'core:AdminPassword',
-  ),
-  'default-sp' => array(
+  ],
+  'default-sp' => [
     'saml:SP',
     'privatekey' => '../certs/saml.pem',
     'certificate' => '../certs/saml.crt',
     'entityid' => 'https://sts.windows.net/a191b859-c4b3-4424-a14d-323b150a30ab/',
     'idp' => 'https://sts.windows.net/a191b859-c4b3-4424-a14d-323b150a30ab/',
-  ),
-);
+  ],
+];
