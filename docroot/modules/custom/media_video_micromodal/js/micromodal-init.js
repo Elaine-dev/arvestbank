4413052ccbaf5ace32f0c51e448d74e6bdd4729e
Scott Earnest <searnest@elevatedthird.com>
2021-05-05 11:07:32 -0600

    AB-262: Fixed bug stopping video on modal close.

--- docroot/modules/custom/media_video_micromodal/js/micromodal-init.js
+++ docroot/modules/custom/media_video_micromodal/js/micromodal-init.js
@@ -1,34 +1,27 @@
 Drupal.behaviors.clnMicroModal = {
+
   attach(context) {
 
     const mm = context.classList && context.classList.contains('modal')
-      ? [context] : context.querySelectorAll('.micromodal');
+      ? [context] : context.querySelectorAll('.modal');
+
     if (mm.length === 0) { return; }
 
-    MicroModal.init();
-
-    document.querySelectorAll('[data-micromodal-trigger]').forEach((link) => {
-      link.addEventListener('click', (ev) => {
-        ev.preventDefault();
-      });
+    MicroModal.init({
+      onClose: function (modal) {
+        resetIframe(modal)
+      },
     });
 
-    document.querySelectorAll('[data-micromodal-close]').forEach((link) => {
-      link.addEventListener('click', (ev) => function ( element ) {
-        var iframe = element.querySelector( 'iframe');
-        var video = element.querySelector( 'video' );
-        if ( iframe !== null ) {
-          var iframeSrc = iframe.src;
-          iframe.src = iframeSrc;
-        }
-        if ( video !== null ) {
-          video.pause();
-        }
-        window.alert('close');
-      });
-    });
-
-
+    // Need this to stop the video on iframe close.
+    function resetIframe(element) {
+      var iframe = element.querySelector('iframe');
+      if ( iframe !== null ) {
+        var iframeSrc = iframe.src;
+        iframe.src = iframeSrc;
+      }
+    }
 
   },
+
 };
