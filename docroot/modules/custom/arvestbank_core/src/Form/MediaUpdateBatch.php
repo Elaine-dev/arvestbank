9a830d7a897ffd755e2eabe651aa9e70cb9e6ff2
Scott Earnest <searnest@elevatedthird.com>
2021-06-15 15:16:42 -0600

    AB-324: Media update now supports batch adding alt/title tags.

--- docroot/modules/custom/arvestbank_core/src/Form/MediaUpdateBatch.php
+++ docroot/modules/custom/arvestbank_core/src/Form/MediaUpdateBatch.php
@@ -5,7 +5,6 @@ namespace Drupal\arvestbank_core\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\media\Entity\Media;
-use Drupal\Core\Database\Connection;
 
 /**
  * Provides a form to start a media update batch.
@@ -28,31 +27,79 @@ class MediaUpdateBatch extends FormBase {
 
     $form['#prefix'] = '<p>This batch will update media.</p>';
 
-    // This checks for how many need to be updated - keeps consistent w/batch.
-    $results = self::mediaResults();
+    $form['update_action'] = [
+      '#title' => 'Update Action:',
+      '#type' => 'select',
+      '#options' => [
+        '' => '--- select ---',
+        'publish' => 'Set unpublished media to published',
+        'alttags' => 'Fill in missing alt and title tags',
+      ],
+      '#ajax' => [
+        'callback' => '::updateAction',
+        'disable-refocus' => FALSE,
+        'event' => 'change',
+        'wrapper' => 'update-action-wrapper',
+        'progress' => [
+          'type' => 'throbber',
+          'message' => $this->t('Please Wait...'),
+        ],
+      ],
+    ];
 
     // Change the text if the update logic is different.
-    $message_text = "This operation will set unpublished media to published.";
-    $message_text .= "<br>There are " . count($results) . ' to update.';
+    $message_text = "Select an action to apply to media:";
 
-    $form['message'] = [
+    $form['action'] = [
+      '#prefix' => '<div id="update-action-wrapper">',
+      '#suffix' => '</div>',
+    ];
+
+    $form['action']['message'] = [
       '#type' => 'markup',
       '#markup' => $message_text,
     ];
 
-    if ($results) {
+    $form['action']['actions'] = [
+      '#type' => 'actions',
+      'submit' => [
+        '#type' => 'submit',
+        '#value' => 'Proceed',
+      ],
+    ];
 
-      $form['actions'] = [
-        '#type' => 'actions',
-        'submit' => [
-          '#type' => 'submit',
-          '#value' => 'Proceed',
-        ],
-      ];
+    return $form;
+
+  }
+
+  /**
+   * Returns information about the action to be preformed.
+   *
+   * @param array $form
+   *   The form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state.
+   *
+   * @return mixed
+   *   return.
+   */
+  public function updateAction(array &$form, FormStateInterface $form_state) {
+
+    if ($selectedAction = $form_state->getValue('update_action')) {
+
+      // This checks for how many need to be updated - keeps consistent w/batch.
+      $results = self::mediaResults($selectedAction);
+
+      if ($results) {
+        $message_text = "This operation will set unpublished media to published.";
+        $message_text .= "<br>There are " . count($results) . ' to update.';
+        $form['action']['message']['#markup'] = $message_text;
+      }
 
     }
 
-    return $form;
+    return $form['action'];
+
   }
 
   /**
@@ -69,15 +116,18 @@ class MediaUpdateBatch extends FormBase {
       'error_message' => t('The process has encountered an error.'),
     ];
 
+    // Get the action value.
+    $selectedAction = $form_state->getValue('update_action');
+
     // Media to update - see function.
-    $results = self::mediaResults();
+    $results = self::mediaResults($selectedAction);
 
     // Array of operations to add to the batch.
     $operations = [];
     foreach ($results as $result) {
       $mid = $result->mid;
       $operations[] = [
-        [$this, 'updateMedia'], [$mid],
+        [$this, 'updateMedia'], [$mid, $selectedAction],
       ];
     }
     $batch['operations'] = $operations;
@@ -94,16 +144,34 @@ class MediaUpdateBatch extends FormBase {
    *
    * @param int $mid
    *   Media id.
+   * @param string $action
+   *   Action to take.
    * @param array $context
    *   Batch context.
    *
    * @throws \Exception
    */
-  public function updateMedia(int $mid, array &$context) {
+  public function updateMedia(int $mid, string $action, array &$context) {
 
     // Load the media, set to published (or whatever update) and save.
     $media = Media::load($mid);
-    $media->set('status', 1);
+
+    switch ($action) {
+
+      case "publish":
+        $media->set('status', 1);
+        break;
+
+      case "alttags":
+        $alt_text = $this::getAltFromName($media->getName());
+        $image_array = $media->get('field_acquiadam_asset_image')->getValue();
+        $image_array[0]['alt'] = $alt_text;
+        $image_array[0]['title'] = $alt_text;
+        $media->set('field_acquiadam_asset_image', $image_array);
+        break;
+
+    }
+
     $media->save();
 
     // Something to send back for batch update status.
@@ -119,17 +187,75 @@ class MediaUpdateBatch extends FormBase {
    * Holds the logic for what exactly we are querying for in the media.
    *
    * Set initially to find unpublished media.
+   *
+   * @param string|null $action
+   *   Action to take.
+   *
+   * @return mixed
+   *   Query of results.
    */
-  public function mediaResults() {
+  public function mediaResults(string $action = NULL) {
 
     $database = \Drupal::database();
 
-    $query = $database->select('media_field_revision', 'r');
-    $query->join('media', 'm', 'r.vid = m.vid');
-    $query->condition('r.status', '0', '=');
-    $query->fields('m', ['mid']);
+    switch ($action) {
+
+      case "publish":
+        $query = $database->select('media_field_revision', 'r');
+        $query->join('media', 'm', 'r.vid = m.vid');
+        $query->condition('r.status', '0', '=');
+        $query->fields('m', ['mid']);
+        break;
+
+      case "alttags":
+        $query = $database->select('media__field_acquiadam_asset_image', 'i');
+        $query->condition('i.field_acquiadam_asset_image_alt', 'NULL', 'IS NULL');
+        $query->addField('i', 'entity_id', 'mid');
+        break;
+
+    }
+
+    // Use this for testing.
+    $query->range(0, 100);
 
     return $query->execute()->fetchAll();
+
+  }
+
+  /**
+   * Converts a filename to a friendly name for alt and title.
+   *
+   * @param string $name
+   *   Filename passed in.
+   *
+   * @return string
+   *   Friendly name.
+   */
+  public function getAltFromName(string $name) : string {
+
+    // Take off the extension.
+    $name_ar = explode('.', $name);
+    array_pop($name_ar);
+
+    // Turn it back into a string, any other .'s will now be spaces.
+    $name = implode(' ', $name_ar);
+
+    // Strip out other characters.
+    $name = str_replace(['-', '_'], ' ', $name);
+
+    // Turn back into an array.
+    $name_ar = explode(' ', $name);
+
+    // Loop through name array and capitalize each part.
+    $name_ar_caps = [];
+    foreach ($name_ar as $name_piece) {
+      $name_ar_caps[] = ucfirst($name_piece);
+    }
+
+    // And send it back to a string.
+    $name = implode(' ', $name_ar_caps);
+
+    return $name;
 
   }
 
