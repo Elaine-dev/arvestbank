cf6a23661c2f634ad988e655b58b02a22f259c85
Scott Earnest <searnest@elevatedthird.com>
2021-06-23 10:53:49 -0600

    AB-86: Reset exposed filters when one is selected/changed.

---
+++ docroot/modules/custom/arvestbank_views/js/debit_cards.js
@@ -0,0 +1,25 @@
+(function ($) {
+  Drupal.behaviors.debitCards = {
+    attach: function (context, settings) {
+      // Reset other exposed filters when one is selected/changed.
+      $(document).once('debit-cards-ajax').ajaxComplete(function (e, xhr, settings) {
+        // Select Category/Theme.
+        $('select[name="field_debit_card_category_target_id"]').change(function () {
+          $('input[name="field_debit_card_keywords_value"]').val('');
+          $('input[name="geolocation_zip"]').val('');
+          $('#views-exposed-form-specialty-debit-cards-specialty-debit-card-block input[value="Filter"].form-submit').trigger('click');
+        });
+        // Change Keywords.
+        $('input[name="field_debit_card_keywords_value"]').change(function () {
+          $('select[name="field_debit_card_category_target_id"]').children("option[value='All']").prop('selected',true).trigger("chosen:updated");
+          $('input[name="geolocation_zip"]').val('');
+        });
+        // Change School Zip Code.
+        $('input[name="geolocation_zip"]').change(function () {
+          $('select[name="field_debit_card_category_target_id"]').children("option[value='All']").prop('selected',true).trigger("chosen:updated");
+          $('input[name="field_debit_card_keywords_value"]').val('');
+        });
+      });
+    }
+  }
+})(jQuery);
