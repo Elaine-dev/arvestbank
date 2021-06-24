382fc6766bf3311e98be96d216f83bc54fa046ff
Allie Ray Jones <allie.jones@acquia.com>
2021-06-24 22:03:40 +0000

    AB-63: Split prod cert in config.

--- simplesamlphp/config/authsources.php
+++ simplesamlphp/config/authsources.php
@@ -11,4 +11,12 @@ $config = [
     'entityid' => 'https://sts.windows.net/a191b859-c4b3-4424-a14d-323b150a30ab/',
     'idp' => 'https://sts.windows.net/a191b859-c4b3-4424-a14d-323b150a30ab/',
   ],
+
+  'prod-sp' => [
+    'saml:SP',
+    'privatekey' => '../certs/saml.pem',
+    'certificate' => '../certs/drupal-prod.crt',
+    'entityid' => 'https://sts.windows.net/a191b859-c4b3-4424-a14d-323b150a30ab/',
+    'idp' => 'https://sts.windows.net/a191b859-c4b3-4424-a14d-323b150a30ab/',
+  ],
 ];
