5b43b296a88c2698f78b90e0ba32adcd6ca548c9
John Cunningham <drfirepants@2954699.no-reply.drupal.org>
2021-08-04 15:06:05 -0400

    AB-000: Refactor so that we request the track-user endpoint only when  a user clicks on ask arvest search.

--- docroot/modules/custom/arvestbank_ask_arvest/js/track-user.js
+++ docroot/modules/custom/arvestbank_ask_arvest/js/track-user.js
@@ -5,11 +5,12 @@
 Drupal.behaviors.arvestbankUserTracking = {
   attach: function (context, settings) {
 
-    // Make request to our tracking endpoint.
-    jQuery.ajax({
-      url: '/ask-arvest/track-user',
-      dataType: "json",
+    jQuery("[data-drupal-selector='edit-search']").click(function(){
+      // Make request to our tracking endpoint.
+      jQuery.ajax({
+        url: '/ask-arvest/track-user',
+        dataType: "json",
+      });
     });
-
   }
 }
